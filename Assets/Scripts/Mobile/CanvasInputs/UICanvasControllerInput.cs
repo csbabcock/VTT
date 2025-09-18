dffba6600765a72e558a3528bb0b363a021e07b1
Vultron <csbabcock@protonmail.com>
2025-09-17 20:21:57 -0400

    Moving files, renaming namespaces, adding git attributes

--- Assets/Scripts/Mobile/CanvasInputs/UICanvasControllerInput.cs
+++ Assets/Scripts/Mobile/CanvasInputs/UICanvasControllerInput.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-namespace StarterAssets
+namespace VTT
 {
     public class UICanvasControllerInput : MonoBehaviour
     {
--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 /* Note: animations are called via the controller for both the character and capsule using animator null checks
  */
 
-namespace StarterAssets
+namespace VTT
 {
     [RequireComponent(typeof(CharacterController))]
 #if ENABLE_INPUT_SYSTEM 
